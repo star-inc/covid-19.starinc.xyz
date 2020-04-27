d2b786a039896cc2b59ebc2756db94036361a01e
SuperSonic <supersonic@livemail.tw>
2020-04-27 22:20:22 +0800

    Update

--- static/js/instant.js
+++ static/js/instant.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 6 */
+// (c) 2020 Star Inc.
 
 $(function () {
     var init_location = [23.730, 120.890];
@@ -8,10 +9,17 @@ $(function () {
         maxZoom: 18,
     }).addTo(map);
     let data_url = "https://pomber.github.io/covid19/timeseries.json";
+    let data2_url = "https://gist.githubusercontent.com/jacobbubu/060d84c2bdf005d412db/raw/845c78f55e49fee89814bdc599355069f07b7ee6/countries.json";
     $.getJSON(data_url, function (xhr) {
-        Object.keys(xhr).forEach(function (e) {
-            var date = Math.max(...Object.keys(xhr[e]));
-            $("#data").append(e + " " + xhr[e][date].confirmed + "<br>");
-        })
+        $.getJSON(data2_url, function (country_names) {
+            Object.keys(xhr).forEach(function (e) {
+                var date = Math.max(...Object.keys(xhr[e]));
+                country_names.forEach(function (country) {
+                    if (country.English.trim() === e) {
+                        $("#data").append(country.Taiwan + " " + xhr[e][date].confirmed + "<br>");
+                    }
+                })
+            })
+        });
     });
-})
+});
