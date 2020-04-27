89718fccb1974e0ae2bb5a9050974ddb77d500d4
SuperSonic <supersonic@livemail.tw>
2020-04-28 00:49:03 +0800

    Update map

---
+++ content/maskmap/wrapper.md
@@ -0,0 +1,9 @@
++++
+fragment = "content"
+date = "2020-04-26"
+weight = 1000
+
+background = "light"
++++
+
+# Come Soon
--- static/js/instant.js
+++ static/js/instant.js
@@ -1,19 +1,42 @@
 /*jshint esversion: 6 */
 // (c) 2020 Star Inc.
 
-$(function () {
-    var init_location = [23.730, 120.890];
-    let map = L.map('map').setView(init_location, 7);
+let map = L.map('map');
+var init_location = [23.730, 120.890];
+let locate_url = "https://gist.githubusercontent.com/erdem/8c7d26765831d0f9a8c62f02782ae00d/raw/248037cd701af0a4957cce340dabb0fd04e38f4c/countries.json";
+
+function setmap() {
+    map.setView(init_location, 3);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '<a href="https://www.openstreetmap.org/">OSM</a>',
         maxZoom: 18,
     }).addTo(map);
+}
+
+function locale(country_name) {
+    map.eachLayer(function (layer) {
+        map.removeLayer(layer)
+    });
+    setmap();
+    $.getJSON(locate_url, function (xhr) {
+        xhr.forEach(function (country) {
+            if (country.name == country_name) {
+                let map_location = country.latlng;
+                map.setView(map_location, 3);
+                L.marker(map_location).addTo(map);
+            }
+        });
+    });
+}
+
+$(function () {
+    setmap();
 
     let data_url = "https://pomber.github.io/covid19/timeseries.json";
     let data2_url = "https://gist.githubusercontent.com/supersonictw/86038eb5cda33229d6367e4f7499e066/raw/63ee5e0afc74fe3542d7155d4201ce0d9046b14e/countries.json";
 
-    function format(country_name, confirmed, recovered, deaths) {
-        return "<tr><th scope=\"row\">" + country_name + "</th><td>" + confirmed + "</td><td>" + recovered + "</td><td>" + deaths + "</td></tr>";
+    function format(origin_name, country_name, confirmed, recovered, deaths) {
+        return "<tr><th scope=\"row\" onclick=\"locale('" + origin_name + "')\">" + country_name + "</th><td>" + confirmed + "</td><td>" + recovered + "</td><td>" + deaths + "</td></tr>";
     }
 
     function time() {
@@ -42,12 +65,12 @@ $(function () {
                     var showed = false;
                     country_names.forEach(function (country) {
                         if (country.English.trim() === name) {
-                            $("#data").append(format(country.Taiwan, xhr[e][date].confirmed, xhr[e][date].recovered, xhr[e][date].deaths));
+                            $("#data").append(format(e, country.Taiwan, xhr[e][date].confirmed, xhr[e][date].recovered, xhr[e][date].deaths));
                             showed = true;
                         }
                     });
                     if (!showed) {
-                        $("#data").append(format(e, xhr[e][date].confirmed, xhr[e][date].recovered, xhr[e][date].deaths));
+                        $("#data").append(format(e, e, xhr[e][date].confirmed, xhr[e][date].recovered, xhr[e][date].deaths));
                     }
                 });
             });
