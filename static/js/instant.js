88f0560ce6e8d2c255543389daae03c17ca0cd56
SuperSonic <supersonic@livemail.tw>
2020-04-27 22:37:20 +0800

    Update

--- static/js/instant.js
+++ static/js/instant.js
@@ -9,17 +9,23 @@ $(function () {
         maxZoom: 18,
     }).addTo(map);
     let data_url = "https://pomber.github.io/covid19/timeseries.json";
-    let data2_url = "https://gist.githubusercontent.com/jacobbubu/060d84c2bdf005d412db/raw/845c78f55e49fee89814bdc599355069f07b7ee6/countries.json";
+    let data2_url = "https://gist.githubusercontent.com/supersonictw/86038eb5cda33229d6367e4f7499e066/raw/63ee5e0afc74fe3542d7155d4201ce0d9046b14e/countries.json";
     $.getJSON(data_url, function (xhr) {
         $.getJSON(data2_url, function (country_names) {
             Object.keys(xhr).forEach(function (e) {
                 var date = Math.max(...Object.keys(xhr[e]));
+                var name = (e === "Taiwan*") ? "Taiwan" : e;
+                var showed = false;
                 country_names.forEach(function (country) {
-                    if (country.English.trim() === e) {
+                    if (country.English.trim() === name) {
                         $("#data").append(country.Taiwan + " " + xhr[e][date].confirmed + "<br>");
+                        showed = true;
                     }
-                })
-            })
+                });
+                if (!showed) {
+                    $("#data").append(e + " " + xhr[e][date].confirmed + "<br>");
+                }
+            });
         });
     });
 });
