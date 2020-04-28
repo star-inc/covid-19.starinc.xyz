faea3fe7814e1dd48571cc3a4e15fcf775c9930b
SuperSonic <supersonic@livemail.tw>
2020-04-28 21:38:19 +0800

    Add chart2

--- static/js/instant.js
+++ static/js/instant.js
@@ -31,8 +31,33 @@ function chart(labels, data) {
     });
 }
 
+function chart2(data) {
+    let info = [(data.confirmed - data.recovered - data.deaths), data.recovered, data.deaths];
+    let ctx = document.getElementById('chart2').getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["treating", "recovered", "deaths"],
+            datasets: [{
+                label: '% of Cases',
+                data: info,
+                borderColor: [
+                    'rgba(220, 0, 130, 0.5)',
+                    'rgba(15, 205, 25, 0.5)',
+                    'rgba(0, 0, 0, 0.5)'
+                ],
+                backgroundColor: [
+                    'rgba(220, 0, 130, 0.5)',
+                    'rgba(15, 205, 25, 0.5)',
+                    'rgba(0, 0, 0, 0.5)'
+                ]
+            }]
+        }
+    });
+}
+
 function format(origin_name, country_name) {
-    return "<h4>" + origin_name + "</h4><p>" + country_name + "</p><div class=\"data-container\"><div id=\"data-date\"></div><canvas id=\"chart\" width=\"100%\" height=\"60px\"></canvas></div><a href=\"javascript:locale('global')\">返回</a>";
+    return "<h4>" + origin_name + "</h4><p>" + country_name + "</p><div class=\"data-container\"><div id=\"data-date\"></div><canvas id=\"chart\" width=\"100%\" height=\"60px\"></canvas><canvas id=\"chart2\" width=\"100%\" height=\"60px\"></canvas></div><a href=\"javascript:locale('global')\">返回</a>";
 }
 
 function locale(country_name) {
@@ -69,6 +94,7 @@ function locale(country_name) {
                 }
                 $("#data-date").text("From " + xhr[country_name][init].date + " to " + xhr[country_name][total - 1].date);
                 chart(Object.keys(data), data);
+                chart2(xhr[country_name][total - 1]);
             });
         }, 300);
     }
@@ -102,12 +128,12 @@ $(function () {
         $.getJSON(data_url, function (xhr) {
             $.getJSON(data2_url, function (country_names) {
                 Object.keys(xhr).forEach(function (e) {
-                    let date = Math.max(...Object.keys(xhr[e]));
+                    let latest = xhr[e].length - 1;
                     let name = (e === "Taiwan*") ? "Taiwan" : e;
                     let showed = false;
                     country_names.forEach(function (country) {
                         if (country.English.trim() === name) {
-                            $("#data").append(format(e, country.Taiwan, xhr[e][date].confirmed, xhr[e][date].recovered, xhr[e][date].deaths));
+                            $("#data").append(format(e, country.Taiwan, xhr[e][latest].confirmed, xhr[e][latest].recovered, xhr[e][latest].deaths));
                             showed = true;
                         }
                     });
